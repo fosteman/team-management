5beb7d1f0e5a243ed35c00b1c2fc2b77f1f7b560
Timofei Shchepkin <dgrz5.0@gmail.com>
2019-06-20 03:17:05 -0400

    Updates

---
+++ precache-manifest.e271f99a37aaf0767ebd06deb19e7ddb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "332da56787eaeb0e8ad4a28aa3d5fa76",
+    "url": "/team-management/index.html"
+  },
+  {
+    "revision": "4bf42c12bc7b793712ae",
+    "url": "/team-management/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "be3735ba3f520d09eeee",
+    "url": "/team-management/static/js/2.13dff2e7.chunk.js"
+  },
+  {
+    "revision": "4bf42c12bc7b793712ae",
+    "url": "/team-management/static/js/main.b69d2992.chunk.js"
+  },
+  {
+    "revision": "7efe0c96e8b3bc07e096",
+    "url": "/team-management/static/js/runtime~main.741f6695.js"
+  }
+]);
