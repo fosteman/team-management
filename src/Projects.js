55e5a8c9c010838c7f3a82e0eeed62c12505546c
Wenxuan Liu <wliu122@myseneca.ca>
2019-07-07 12:07:49 -0400

    -- refactored Teams,Project Component using material-ui

--- src/Projects.js
+++ src/Projects.js
@@ -1,32 +1,36 @@
 import React from 'react';
 import MainContainer from './MainContainer';
 import moment from 'moment';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Typography from '@material-ui/core/Typography';
 
 const Projects = ({projects, title}) => (
     <MainContainer sidebar={title}>
-        <h1 className="page-header">{title}</h1>
-        <div className="row">
-                <table className="table table-striped table-bordered">
-                    <tbody>
-                        <tr>
-                            <th>Name</th>
-                            <th>Description</th>
-                            <th>Start Date</th>
-                            <th>End Date</th>
-                        </tr>
-                        {projects.map((project, index) => {
-                            return (
-                                <tr>
-                                    <td key={index}>{project.ProjectName}</td>
-                                    <td key={index}>{project.ProjectDescription}</td>
-                                    <td key={index}>{moment(project.ProjectStartDate).format('LL')}</td>
-                                    <td key={index}>{(project.ProjectEndDate == null ? 'n/a' : moment(project.ProjectEndDate).format('LL'))}</td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
-            </div>
+        < Typography variant={'h1'}>{title}</Typography>
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>Name</TableCell>
+                    <TableCell>Description</TableCell>
+                    <TableCell>Start Date</TableCell>
+                    <TableCell>End Date</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {projects.map((project, index) => (
+                    <TableRow>
+                        <TableCell key={index}> {project.ProjectName} </TableCell>
+                        <TableCell key={index}>{project.ProjectDescription}</TableCell>
+                        <TableCell key={index}>{moment(project.ProjectStartDate).format('LL')}</TableCell>
+                        <TableCell key={index}>{(project.ProjectEndDate == null ? 'n/a' : moment(project.ProjectEndDate).format('LL'))}</TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
     </MainContainer>
 );
 export default Projects
--- src/Teams.js
+++ src/Teams.js
@@ -1,27 +1,37 @@
 import React from 'react';
 import MainContainer from './MainContainer';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import {Typography} from "@material-ui/core";
 
 const Teams = ({teams, title}) => (
     <MainContainer sidebar={title}>
-        <h1 className={"page-header"}>{title}</h1>
-        <table className={"table table-striped table-bordered"}>
-            <tbody>
-                <tr>
-                    <th>Name</th>
-                    <th>Projects</th>
-                    <th>Employees</th>
-                    <th>Team Lead</th>
-                </tr>
+        < Typography variant={'h1'}>{title}</Typography>
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>Name</TableCell>
+                    <TableCell>Projects</TableCell>
+                    <TableCell>Employees</TableCell>
+                    <TableCell>Team Lead</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
                 {teams.map((team, index) => (
-                        <tr>
-                            <td key={index}>{team.TeamName}</td>
-                            <td key={index}>{team.Projects.map((project, index) => <li key={index}>{project.ProjectName}</li>)}</td>
-                            <td key={index}>{team.Employees.lenght} Employees</td>
-                            <td key={index}>{team.TeamLead.FirstName} {team.TeamLead.LastName}</td>
-                        </tr>
+                        <TableRow>
+                            <TableCell key={index}>{team.TeamName}</TableCell>
+                            <TableCell key={index}>{team.Projects.map((project, index) => <li key={index}>{project.ProjectName}</li>)}</TableCell>
+                            <TableCell key={index}>{team.Employees.lenght} Employees</TableCell>
+                            <TableCell key={index}>{team.TeamLead.FirstName} {team.TeamLead.LastName}</TableCell>
+                        </TableRow>
                     ))}
-            </tbody>
-        </table>
+            </TableBody>
+        </Table>
     </MainContainer>
 );
 
