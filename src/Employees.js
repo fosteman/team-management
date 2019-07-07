e6fbc47118488d4352011ca8ff828e42d63656f8
Timofei Shchepkin <dgrz5.0@gmail.com>
2019-07-07 11:30:15 -0400

    materialized employees

--- src/Employees.js
+++ src/Employees.js
@@ -1,31 +1,41 @@
 import React from 'react';
 import MainContainer from './MainContainer';
 import moment from 'moment';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+
 const Employees = ({employees, title}) => (
     <MainContainer sidebar={title}>
-        <h1 className="page-header">{title}</h1>
-        <table className="table table-striped table-bordered">
-            <tbody>
-                <tr>
-                    <th>Name & Position</th>
-                    <th>Addresss</th>
-                    <th>Phone Num</th>
-                    <th>Hire Date</th>
-                    <th>Salary Bonus</th>
-                </tr>
-                {employees.map((employee, index) => {
-                    return (
-                        <tr>
-                            <td key={index}>{employee.FirstName} {employee.LastName}-{employee.Position.PositionName}</td>
-                            <td key={index}>{employee.AddressStreet}, {employee.AddressCity}, {employee.AddressState}, {employee.AddressZip}</td>
-                            <td key={index}>{employee.PhoneNum} ext {employee.Extension}</td>
-                            <td key={index}>{moment(employee.HireDate).format('LL')}</td>
-                            <td key={index}>${employee.SalaryBonus}</td>
-                        </tr>
-                    );
-                })}
-            </tbody>
-        </table>
+        <h1>{title}</h1>
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>Name & Position</TableCell>
+                    <TableCell>Address</TableCell>
+                    <TableCell>Phone #</TableCell>
+                    <TableCell>Hire Date</TableCell>
+                    <TableCell>Salary Bonus</TableCell>
+                </TableRow>
+            </TableHead>
+
+            <TableBody>
+                {employees.map((employee, index) => (
+                    <TableRow>
+                        <TableCell key={index}>{employee.FirstName} {employee.LastName}-{employee.Position.PositionName}</TableCell>
+                        <TableCell key={index}>{employee.AddressStreet}, {employee.AddressCity}, {employee.AddressState}, {employee.AddressZip}</TableCell>
+                        <TableCell key={index}>{employee.PhoneNum} ext {employee.Extension}</TableCell>
+                        <TableCell key={index}>{moment(employee.HireDate).format('LL')}</TableCell>
+                        <TableCell key={index}>${employee.SalaryBonus}</TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
     </MainContainer>
 );
 export default Employees
