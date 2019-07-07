5781f4e3fea40b5fbf2d300b8feaf65e2d7d562f
Timofei Shchepkin <dgrz5.0@gmail.com>
2019-07-07 16:14:29 -0400

    MainContainer, NavigationBar, and Side Drawer

--- src/MainContainer.js
+++ src/MainContainer.js
@@ -1,17 +1,56 @@
 import React from 'react';
 import NavBar from './NavBar';
 import SideBar from './SideBar';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Divider from '@material-ui/core/Divider';
+import CssBaseline from '@material-ui/core/CssBaseline';
+const drawerWidth = 240;
 
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+    },
+    appBar: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    toolbar: theme.mixins.toolbar,
+    content: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.default,
+        padding: theme.spacing(),
+    },
+}));
 
-const MainContainer = ({sidebar, children}) => (
-    <Paper>
-        <NavBar/>
-        <SideBar highlight={sidebar} />
-        <Grid container spacing={3}>
-            <Grid className="main" xs={6}>{children}</Grid>
-        </Grid>
-    </Paper>
-);
+const MainContainer = ({sidebar, children}) => {
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <NavBar/>
+            <Drawer
+                className={classes.drawer}
+                variant="permanent"
+                classes={{paper: classes.drawerPaper}}
+                anchor="left"
+            >
+                <div className={classes.toolbar} />
+                <Divider />
+                <SideBar />
+                <Divider />
+            </Drawer>
+            <main className={classes.content}>
+                {children}
+            </main>
+        </div>
+    );
+};
 export default MainContainer
--- src/SideBar.js
+++ src/SideBar.js
@@ -1,28 +1,15 @@
-import React, { Component, useState } from 'react';
-import {Link} from 'react-router-dom';
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 
-export default function SideBar(props){
-    const [highlight, setHighlight] = useState(false);
-    return (
-        <div className="col-sm-3 col-md-2  sidebar"  onClick={() => setHighlight(true)}>
-            <ul className="nav nav-sidebar">
-                <li className={(props.highlight === "Overview" ? 'active' : '')}>
-                    <Link to="/">Overview
-                        <span className="sr-only">(current)</span>
-                    </Link>
-                </li>
-            </ul>
-            <ul className="nav nav-sidebar">
-                <li className={(props.highlight === "Projects" ? 'active' : '')}>
-                    <Link to="/projects">Projects</Link>
-                </li>
-                <li className={(props.highlight === "Teams" ? 'active' : '')}>
-                    <Link to="teams">Teams</Link>
-                </li>
-                <li className={(props.highlight === "Employees" ? 'active' : '')}>
-                    <Link to="/employees">Employees</Link>
-                </li>
-            </ul>
-        </div>
-    );
-}
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+
+const SideBar = (props) => (
+    <List>
+        <ListItem component={RouterLink} to="/" button>Overview</ListItem>
+        <ListItem component={RouterLink} to="/projects" button>Projects</ListItem>
+        <ListItem component={RouterLink} to="/teams" button>Teams</ListItem>
+        <ListItem component={RouterLink} to="/employees" button>Employees</ListItem>
+    </List>
+);
+export default SideBar
