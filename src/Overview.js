314365e8a015a4fbcd8fa16fd39c100c76c144a7
Timofei Shchepkin <dgrz5.0@gmail.com>
2019-07-11 05:04:50 -0400

    Materialized Overview Grid container

--- src/Overview.js
+++ src/Overview.js
@@ -1,24 +1,26 @@
-import React, { Component } from 'react';
-import MainContainer from './MainContainer';
-import ProjectPanel from './ProjectPanel';
-import TeamsPanel from './TeamsPanel';
-import EmployeesPanel from './EmployeesPanel';
+import React from 'react'
+import MainContainer from './MainContainer'
+import ProjectPanel from './ProjectPanel'
+import TeamsPanel from './TeamsPanel'
+import EmployeesPanel from './EmployeesPanel'
+import Typography from "@material-ui/core/Typography"
+import Grid from "@material-ui/core/Grid";
+
 
 const Overview = ({data, title}) => (
-
     <MainContainer sidebar={title}>
-        <h1 className="page-header">{title}</h1>
-            <div className="row">
-                <div className="col-md-4">
+        <Typography variant={'h2'}>{title}</Typography>
+            <Grid container spacing={3}>
+                <Grid item xs={4}>
                     <ProjectPanel projects={data.projects} title="Projects"/>
-                </div>
-                <div className="col-md-4">
+                </Grid>
+                <Grid item xs={4}>
                     <TeamsPanel teams={data.teams} title="Teams"/>
-                </div>
-                <div className="col-md-4">
+                </Grid>
+                <Grid item xs={4}>
                     <EmployeesPanel employees={data.teams} title="Employees"/>
-                </div>
-        </div>
+                </Grid>
+        </Grid>
     </MainContainer>
 );
-export default Overview;
+export default Overview
